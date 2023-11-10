7577602eca1d9619ce1872741c5457f9536097ef
chullin <fg6ts15@gmail.com>
2023-11-10 15:27:13 +0800

    Using python for the simplest stacks

---
+++ stack.py
@@ -0,0 +1,39 @@
+Max = 10
+Stack = [None] * Max
+Top = 0
+
+def isFull():
+    global Top
+    global Max
+    if(Top > Max):
+        return True
+    else:
+        return False
+    
+def push(new_data):
+    global Top
+    if(isFull() != True):
+        Stack[Top] = new_data   # push data in stack
+        Top = Top + 1           # The top has to be moved up  
+    
+def isEmpty():
+    global Top
+    if(Top == 0):
+        return True
+    else:
+        return False
+
+def pop():
+    global Top
+    global Max
+    if(isEmpty() != True):
+        print(f"頂部資料為:{Stack[Top-1]}")
+        Stack.pop()
+        Top = Top - 1
+
+push(10)
+push(15)
+push(100)
+pop()
+pop()
+pop()
