eeb11fe5d00cbc2d5faafff31f882ae72730aecb
chullin <fg6ts15@gmail.com>
2023-12-12 18:10:52 +0800

    complete simple DP program

--- practice for Python/Dynamic Programming/DP_FibonacciNumber.py
+++ practice for Python/Dynamic Programming/DP_FibonacciNumber.py
@@ -1,6 +1,21 @@
-f = [0 for in range(100)]
+f = [0 for i in range(100)]
 
 def fibo(n):
     if f[n]:
+        # print("f[n] = ",f[n])
         return f[n]
-    if n
+    if n <= 1:
+        return n
+    f[n] = fibo(n-1) + fibo(n-2)
+    return f[n]
+
+
+n = 45
+for i in range(n+1):
+    print(fibo(i))
+    # fibo(i)
+# print()
+
+
+# for i in range(45):
+#     print(f[i])
