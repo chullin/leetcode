79d0d263550bb73b85d8828a50e8599b323ee2ed
chullin <fg6ts15@gmail.com>
2024-08-25 09:38:30 +0800

    更新連結，原先的短連結失效

---
+++ NeetCode 150/Duplicate Integer/Duplicate Integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hasDuplicate(self, nums: list[int]) -> bool:
+         
+         pass
+
+
+
+Solution_instance = Solution()
+
+nums1 = [1, 2, 3, 3]
+nums2 = [1, 2, 3, 4]
+
+result1 = Solution_instance.hasDuplicate(nums1)
+result2 = Solution_instance.hasDuplicate(nums2)
+
+print("Result1:", result1)
+print("Result2:", result2)
