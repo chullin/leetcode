f0d685f123665eeccdf73ebc10e42ca5ba827725
chullin <fg6ts15@gmail.com>
2024-09-28 21:05:33 +0800

    Best Time to Buy And Sell Stock 的解答

--- NeetCode 150/Best Time to Buy And Sell Stock/Best Time to Buy And Sell Stock.py
+++ NeetCode 150/Best Time to Buy And Sell Stock/Best Time to Buy And Sell Stock.py
@@ -1,3 +1,21 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        
+    def maxProfit(self, prices: list[int]) -> int:
+        min = prices[0]
+        profit = 0
+        for i in prices:
+            if i < min:
+                min = i
+            else:
+                profit = max(profit, i - min)
+        return profit
+
+Solution_instance = Solution()
+
+Input1 = [10,1,5,6,7,1]
+Input2 = [10,8,7,5,2]
+
+result1 = Solution_instance.maxProfit(Input1)
+result2 = Solution_instance.maxProfit(Input2)
+
+print("Result1:", result1)
+print("Result2:", result2)
